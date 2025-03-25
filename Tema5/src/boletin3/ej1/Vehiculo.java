de44b38ab873ab440923eea130c0a3e8f5775424
LauraLuzhen <ll.rodriguez@iesnervion.es>
2025-03-25 14:37:42 +0100

    otro

--- Tema5/src/boletin3/ej1/Vehiculo.java
+++ Tema5/src/boletin3/ej1/Vehiculo.java
@@ -10,5 +10,26 @@ public class Vehiculo {
 	private int marcha = 0;
 	private double velocidad;
 	
+	public void parar() {
+		motorEncendido = false;
+		marcha = 0;
+		velocidad = 0;
+	}
+	
+	public void arrancar() {
+		motorEncendido = true;
+		marcha = 0;
+		velocidad = 0;
+	}
+	
+	public boolean subirMarcha() {
+		boolean realizado = false;
+		if (marcha + 1 <= 5) {
+			marcha++;
+			realizado = true;
+		} 
+		return realizado;
+	}
+	
 	
 }
